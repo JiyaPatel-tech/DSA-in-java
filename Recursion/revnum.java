b850dd3ae84b0a6c25556e8f358155f248858c6a
JiyaPatel-tech <jiyamkpatel01@gmail.com>
2026-03-03 19:40:35 +0530

    commit

---
+++ Recursion/revnum.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class revnum {
+    public static void main(String[] args) {
+        revnumber(5);
+    }
+    static void revnumber(int n)
+    {
+        if(n==0){
+            return;
+        }
+        revnumber(n-1);
+        System.out.println(n);
+    }
+}
